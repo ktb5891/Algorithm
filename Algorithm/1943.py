066adb3ef54fe1fdc33449c0eb51654a051fbcdd
ktb5891 <68469395+ktb5891@users.noreply.github.com>
2021-03-17 16:01:52 +0900

    Add files via upload
    동전 분배
    다이나믹 프로그래밍
    배낭문제

--- Algorithm/1943.py
+++ Algorithm/1943.py
@@ -1,50 +1,29 @@
 ans = []
+
 for _ in range(3):
+    money = 0
+    dp = [0 for i in range(50001)]
+    coin = [[] for i in range(101)]
     num = int(input())
-    dic = dict()
-    for i in range(num):
-        coin, x = map(int, input().split())
-        dic[coin] = x
-    # print(dic[coin])
-    sort_dic = sorted(dic.items())
-    # print(sort_dic)
-    money = [0,0]
-    while sort_dic:
-        k = list(sort_dic.pop())
-        # print(k)
-        if money[0] == money[1]:
-            if k[1]%2 == 0:
-                money[0] += k[0]*(k[1]/2)
-                money[1] += k[0]*(k[1]/2)
-            else:
-                while k[1] > 0:
-                    if money[0] == money[1]:
-                        money[0] += k[0]
-                        k[1] -= 1
-                    elif money[0] > money[1]:
-                        money[1] += k[0]
-                        k[1] -= 1
-                    elif money[0] < money[1]:
-                        money[0] += k[0]
-                        k[1] -= 1
-        else:
-            while k[1] > 0:
-                    if money[0] == money[1]:
-                        money[0] += k[0]
-                        k[1] -= 1
-                    elif money[0] > money[1]:
-                        money[1] += k[0]
-                        k[1] -= 1
-                    elif money[0] < money[1]:
-                        money[0] += k[0]
-                        k[1] -= 1
+    # print(coin)
+    for i in range(1,num+1):
+        a, b = map(int,input().split())
+        money += a*b
+        coin[i] = [a,b]
+    # print(coin)
     # print(money)
-    if money[0] == money[1]:
-        ans.append(1)
-        # print(ans)
-    else:
+    if money%2 == 1:
         ans.append(0)
-        # print(ans)
-    
+    else:
+        dp[0] = 1
+        for i in range(1,num+1,1):
+            for j in range(50000,coin[i][0]-1,-1):
+                if dp[j-coin[i][0]]:
+                    k=0
+                    while k < coin[i][1] and j+k*coin[i][0] <= 50000:
+                        dp[j+k*coin[i][0]] = 1
+                        k += 1
+        ans.append(dp[int(money/2)])
+        # print(dp)
 for i in ans:
     print(i)
